26fe088161934b1c265a11f307f97708c383c124
MrAkashTyagi <tyagiakash94@gmail.com>
2026-05-09 14:01:04 +0530

    communication between services using rest template

--- UserService/src/main/java/com/bigsquare/user/service/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/bigsquare/user/service/impl/UserServiceImpl.java
@@ -1,21 +1,31 @@
 package com.bigsquare.user.service.impl;
 
+import com.bigsquare.user.service.entities.Rating;
 import com.bigsquare.user.service.entities.User;
 import com.bigsquare.user.service.exceptions.ResourceNotfoundException;
 import com.bigsquare.user.service.repositories.UserRepository;
 import com.bigsquare.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Override
     public User saveUser(User user) {
@@ -27,11 +37,25 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> getAllUser() {
+//        implement rating service call using rest template
         return userRepository.findAll();
     }
 
+//    get single user
     @Override
     public User findUserById(String id) {
-        return userRepository.findById(id).orElseThrow(()->new ResourceNotfoundException("User with given id is not found on the server"+id));
+//get user from the database with use of user repository
+        User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotfoundException("User with given id is not found on the server" + id));
+
+//        fetch ratings of the above user from RATING SERVICE
+//        localhost:8083/ratings/users/a82d7dbb-96f4-4a80-86d9-2d7562011dfd
+
+        RestTemplate restTemplate = new RestTemplate();
+        ArrayList<Rating> ratingsOfUser = restTemplate.getForObject("http://localhost:8083/ratings/users/"+user.getUserId(), ArrayList.class);
+        logger.info("forObject: {}", ratingsOfUser);
+
+        user.setRatings(ratingsOfUser);
+
+        return user;
     }
 }
