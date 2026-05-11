15c8c121024b0e72f9931a1669ad9f2b696cde42
MrAkashTyagi <tyagiakash94@gmail.com>
2026-05-11 17:05:15 +0530

    configured circuit breaker pattern

--- UserService/src/main/java/com/bigsquare/user/service/controllers/UserController.java
+++ UserService/src/main/java/com/bigsquare/user/service/controllers/UserController.java
@@ -5,13 +5,19 @@ import com.bigsquare.user.service.impl.UserServiceImpl;
 
 import java.util.List;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserServiceImpl userServiceImpl;
@@ -25,9 +31,23 @@ public class UserController {
 
 //    single user get
 
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     @GetMapping("/{userId}")
     public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
         return ResponseEntity.ok(this.userServiceImpl.findUserById(userId));
+    }
+
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception ex) {
+
+        logger.info("Fallback execute ho raha hai kyunki service down hai: {} ", ex.getMessage());
+        User user = User.builder()
+                .name("Dummy User")
+                .about("This dummy user is created because some service is down !!")
+                .email("dummy@gmail.com")
+                .userId("13123")
+                .build(); // Dummy user ya partial data
+//        user.setName("Dummy User (Service Down)");
+        return new ResponseEntity<User>(user,HttpStatus.OK);
     }
 
 //    all user get
